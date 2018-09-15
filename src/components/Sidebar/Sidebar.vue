be4cced4f4a9305a33a93943a0f4e9146f3e21b5
cl4pper <cl4pper@gmail.com>
2018-09-15 13:02:11 -0300

    sidemenu desktop and mobile

---
+++ src/components/Sidebar/Sidebar.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="sidebar">
+
+		<!-- DESKTOP VIEW -->
+		<div class="sidebar-desktop">
+			<div class="sidebar-header">{{ title }}</div>
+			<div class="sidemenu-content">
+				<div class="item"><i class="fas fa-home"></i>Início</div>
+				<div class="item"><i class="fas fa-search"></i>Busca</div>
+				<div class="item"><i class="fas fa-users"></i>Perfil</div>
+			</div>
+		</div>
+
+		<!-- MOBILE VIEW -->
+		<div class="sidebar-mobile">
+			<div class="sidebar-content">
+				<div class="item"><i class="fas fa-bars"></i></div>
+				<div class="item title">Politicos</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'sidebar',
+	props: {
+		title: {
+			type: String,
+			default: "Titulo",
+			required: false,
+		}
+	}
+}
+</script>
+
+<style lang="scss">
+@import './sidebar.scss';
+</style>
+
